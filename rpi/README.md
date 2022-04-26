b78cbbfdc21377d60ddc2c786afb4b8e2b362cd0
wommy <tom@technomad.media>
2022-04-25 20:21:26 -0400

    pwm fan control working!!

---
+++ rpi/README.md
@@ -0,0 +1,8 @@
+# pwm fan control
+
+from 
+
+- https://github.com/torfsen/python-systemd-tutorial
+- https://tecadmin.net/setup-autorun-python-script-using-systemd/
+- https://stackoverflow.com/questions/7323859/python-how-to-call-bash-commands-with-pipe
+- https://stackoverflow.com/questions/4256107/running-bash-commands-in-python
--- rpi/pwm-fan-control.py
+++ rpi/pwm-fan-control.py
@@ -1,3 +1,5 @@
+# armor pwm fan control
+
 import RPi.GPIO as GPIO
 import time
 import subprocess
@@ -12,7 +14,15 @@ pwm.start(dc)
 
 try:
     while True:
-        temp = subprocess.getoutput("vcgencmd measure_temp|sed 's/[^0-9.]//g'")
+        # from https://stackoverflow.com/a/68323238
+        # proc1 = subprocess.run(["vcgencmd measure_temp"], stdout=subprocess.PIPE, shell=True)
+        # proc2 = subprocess.run(["sed 's/[^0-9.]//g'"], input=proc1.stdout, stdout=subprocess.PIPE, shell=True)
+        # temp = proc2.stdout.decode()
+        temp = subprocess.run(["vcgencmd measure_temp | sed 's/[^0-9.]//g'"], shell=True, stdout=subprocess.PIPE).stdout.decode()
+        if round(float(temp)) >= 55:
+            dc = 100
+            pwm.ChangeDutyCycle(dc)
+            time.sleep(120)
         if round(float(temp)) >= 50:
             dc = 70
             pwm.ChangeDutyCycle(dc)
