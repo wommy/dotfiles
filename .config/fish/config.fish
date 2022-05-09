cdb9c78e07068127530ddc2b82b5689f04c7b005
wommy <tom@technomad.media>
2022-05-09 18:48:11 -0400

    fish: greeting & pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx PNPM_HOME "/home/$USER/.local/share/pnpm"
+set -x fish_greeting
+set -gx PNPM_HOME "/home/wom/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
