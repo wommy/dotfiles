22f8f8252bc95108956d350cc508de7b26da8a6e
wommy <tom@technomad.media>
2023-11-20 17:11:35 -0500

    nvim.opts: to lua from vimscript

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,24 @@
+--- settings , options
+	vim.opt.number=true
+	vim.opt.relativenumber=true
+	vim.opt.wrap=false
+	vim.opt.shiftwidth=2
+	vim.opt.tabstop=2
+	vim.opt.smartindent=true
+	vim.opt.scrolloff=999
+	vim.opt.clipboard='unnamedplus'
+	vim.opt.breakindent=true
+	vim.opt.breakindentopt='shift:2,min:40,sbr'
+	vim.opt.showbreak='>>'
+	vim.opt.foldmethod='indent'
+	vim.opt.foldlevelstart=3
+	vim.opt.foldlevel=3
+	vim.opt.laststatus=3
+	vim.opt.mouse='a'
+	vim.opt.confirm=true
+	vim.opt.cmdheight=2
+---
 vim.cmd [[
-""" settings , options
-	set number relativenumber
-	set nowrap
-	set shiftwidth=2 tabstop=2 smartindent
-	set scrolloff=999
-	set clipboard+=unnamedplus
-	set breakindent breakindentopt=shift:2,min:40,sbr showbreak=>> 
-	set foldmethod=indent foldlevelstart=3 foldlevel=3
-	set laststatus=3
-	set mouse=a
-	set confirm
-	set cmdheight=2
-"""
-
 """ plug
 	call plug#begin('~/.config/nvim/plugged')
 	" Plug 'sheerun/vim-polyglot'
