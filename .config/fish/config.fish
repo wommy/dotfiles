f574ffe25df4ca468a6b062a785cd97bcc69c27b
wommy <tom@technomad.media>
2024-08-05 16:41:54 -0400

    fish: bat, local_share

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,9 +8,13 @@ set -x fish_greeting
 abbr -a rsync rsync --info=stats1,progress2 --modify-window=1 -vzhPiSrn
 abbr -a ls eza -a --git --group-directories-first --git-ignore
 abbr -a ll eza -a --git --group-directories-first --git-ignore --tree -L 3
+abbr -a cat bat
+
+## local share var
+set -x LOCAL_SHARE $HOME/.local/share 
 
 ## pnpm
-set -x PNPM_HOME $HOME/.local/share/pnpm
+set -x PNPM_HOME $LOCAL_SHARE/pnpm
 set -x PATH $PATH $PNPM_HOME
 ## pnpm end
 ## rust
@@ -18,8 +22,9 @@ set -x CARGO $HOME/.cargo
 set -x PATH $PATH $CARGO/bin
 ## go
 set -x GOROOT /usr/local/go
-set -x GOBIN /usr/local/go/bin
+set -x GOBIN usr/local/go/bin
 set -x PATH $PATH $GOBIN
 # zig
-set -x ZIG_PATH $HOME/.local/share/zig-0.12.0/
-set -x PATH $PATH $ZIG_PATH
+set -x PATH $PATH $LOCAL_SHARE/zig-0.12.0/
+## bun
+set -x PATH $PATH $HOME/.bun/bin 
