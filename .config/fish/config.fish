d0c23c0c4bb2ef286583849817a85e34fe53570a
wommy <tom@technomad.media>
2024-10-07 10:16:33 -0400

    fish: updated abbr's n install script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,23 +5,16 @@ end
 set -x fish_greeting
 
 ## ABBRs newer, native
-abbr -a rsync rsync --info=stats1,progress2 --modify-window=1 -vzhPiSrn
+abbr -a rsync rsync --info=stats1,progress2 --modify-window=1 -vzhPiSr -n
 abbr -a diff git diff --color-words
 
 ### programs derrived
-### cargo install eza ; cargo install --locked bat ; cargo install zoxide --locked ; nala install thefuck -y ; git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ; ~/.fzf/install ;
-###
-## cargo install eza ;
-abbr -a ls eza --group-directories-first -a --git --git-ignore --tree -L 1
-## cargo install --locked bat ;
-abbr -a cat bat --paging=never
-## git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ; ~/.fzf/install ;
+### nala install build-essential ; curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh; cargo install eza ; cargo install --locked bat ; cargo install zoxide --locked ; git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ; ~/.fzf/install ;
+abbr -a lss eza --group-directories-first -a --git --git-ignore --tree -L 1
+abbr -a catt bat --paging=never
 fzf --fish | source
 abbr -a fzfp fzf --preview \'bat --style numbers --colors always {}\'
-## cargo install zoxide --locked ;
 zoxide init fish | source
-## nala install thefuck -y ;
-# thefuck --alias tf | source
 
 ## local share var
 set -x LOCAL_SHARE $HOME/.local/share 
@@ -34,7 +27,8 @@ set -x PATH $PATH $PNPM_HOME
 set -x CARGO $HOME/.cargo
 set -x PATH $PATH $CARGO/bin
 ## go
-set -x GOROOT /usr/local/go
+# set -x GOROOT /usr/local/go
+set -x GOROOT $LOCAL_SHARE/go
 set -x GOBIN $GOROOT/bin
 set -x PATH $PATH $GOBIN
 set -x GOPATH $LOCAL_SHARE/go
